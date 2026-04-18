3256e06038efc38fa9cbe9d0193b50935501ceff
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-18 15:36:26 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,16 @@
+def sieve_of_eratosthenes(n):
+    if n < 2:
+        return []
+
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+
+    return [i for i in range(2, n + 1) if sieve[i]]
+
+n = int(input("N ni kiriting: "))
+print(sieve_of_eratosthenes(n))
